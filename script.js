77f4f035bc06b8437c386163143c6e93bc4eee57
zacharyandersen <55568872+zacharyandersen@users.noreply.github.com>
2019-12-10 21:16:41 -0600

    Add files via upload
    Adjusted a few HTML values; got my Google Domain to successfully host the website

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ $(document).ready(function() {
     {
       id: 3,
       weight: 0,
-      text: "No girls allowed. THE END."
+      text: "You see yourself in the reflection, your hair is long and unkempt. How long were you out? What are you doing here? You think, trying to at least remember your name."
     },
     {
       id: 4,
@@ -53,7 +53,7 @@ $(document).ready(function() {
       text:
         "That's right, your name is " +
         name +
-        ". Remembering that brings you a small sense of comfort. Finally feeling like your senses and awareness have returned, you decide what your first step is in figuring out what in the world is going on.",
+        ". Remembering that brings you a small sense of comfort. Starting to feel like your senses and awareness have returned, you decide what your first step will be in this strange situation.",
       paths: [
         { target: 5, text: "Walk through the clearing in the trees around you." },
         { target: 6, text: "Rip down the branches around you. You owe nothing to this place and nature is currently in your way." }
@@ -188,12 +188,10 @@ $(document).ready(function() {
 
   function evil(id) {
     evilCounter += stories[id].weight;
-    window.alert("Evil count: " + evilCounter);
   }
 
   function secret(id) {
     secretCounter += stories[id].weight;
-    window.alert("Secret count: " + secretCounter);
   }
 
   // Event handler that will setup and invoke displayInteractiveStory()
